7eda931d909ab44311f83fea66c6cf4e08d7b31d
AppleSeville-UCM <appleseville80@gmail.com>
2025-09-16 09:05:25 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,19 @@
+/**
+ * This is a documentation comment.
+ * It describes the purpose of the class.
+ * You can generate JavaDocs from this comment.
+ */
+public class Comments {
+    public static void main(String[] args) {
+        
+        // This is a single-line comment
+        System.out.println("Hello, this program shows different comments in Java.");
+        
+        /* 
+         This is a multi-line comment.
+         You can write explanations here 
+         spanning multiple lines.
+        */
+        System.out.println("Comments help make code more readable.");
+    }
+}
